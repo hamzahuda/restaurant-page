489e13872b14acef9efcd3e023779deeadffa524
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-07-22 11:14:05 +0100

    Add source mapping and components file tree

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import "./reset.css";
 import "./style.css";
+
+window.addEventListener("DOMContentLoaded", () => {
+    // Create header
+    const header = document.createElement("div");
+    header.classList.add("header");
+
+    document.body.pepend(header);
+});
